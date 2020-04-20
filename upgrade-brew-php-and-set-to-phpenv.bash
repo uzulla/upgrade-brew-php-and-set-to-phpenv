aa403928977ade432ec7cdae87b1b6b01db02ac8
uzulla <uzulla@himitsukichi.com>
2020-04-20 22:44:02 +0900

    fix #4

--- upgrade-brew-php-and-set-to-phpenv.bash
+++ upgrade-brew-php-and-set-to-phpenv.bash
@@ -7,8 +7,14 @@ PHPENV_VERSIONS_DIR=`echo $PHPENV | sed -e 's/bin\/phpenv//'`versions/
 echo $PHPENV_VERSIONS_DIR;
 
 BREW_PHP_DIR=`brew ls php |grep -e '/bin/php$' | sed -e 's/\/bin\/php//'`
+echo BREW_PHP_DIR $BREW_PHP_DIR 
 BREW_PHP_VERSION=`echo $BREW_PHP_DIR  | perl -ne '/([0-9]\.[0-9]\.[0-9])/; print $1;'`
-echo $BREW_PHP_VERSION
+echo BREW_PHP_VERSION $BREW_PHP_VERSION
+BREW_PHP_PATH=`echo $BREW_PHP_DIR  | perl -ne '/([0-9]\.[0-9]\.[0-9]_?[0-9]*)/; print $1;'`
+echo BREW_PHP_PATH $BREW_PHP_PATH
+
+echo ln -sf $BREW_PHP_DIR $PHPENV_VERSIONS_DIR$BREW_PHP_VERSION
+ln -sf $BREW_PHP_DIR $PHPENV_VERSIONS_DIR$BREW_PHP_VERSION
 
 phpenv global $BREW_PHP_VERSION
 
